5b11869ab6a769c29d667e7428e9f0fe68921b35
bus503 <bus503@daum.net>
2020-04-02 19:16:57 +0900

    깃 복구 이후 커밋테스트

--- src/com/yi/dao/impl/MemberDaoImpl.java
+++ src/com/yi/dao/impl/MemberDaoImpl.java
@@ -39,14 +39,12 @@ public class MemberDaoImpl implements MemberDao {
 		String sql = "select mber_id, mber_pass, mber_name, mber_brthdy, mber_zip, mber_bass_ad, mber_detail_ad, mber_tel, mber_img, total_le_cnt, lend_book_cnt, grade, grad_name, book_le_cnt, lend_psb_cdt, join_dt, wdr_cdt,od_cnt from member m left join grade g on m.grade = g.grade_no where mber_id = ?";
 		try (Connection con = JDBCUtil.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql);) {
 			pstmt.setString(1, member.getMberId());
-//			LogUtil.prnLog(pstmt);
-			try (ResultSet rs = pstmt.executeQuery()) {
+			try (ResultSet rs = pstmt.executeQuery()){
 				if (rs.next()) {
 					return getMemberRentCnt(rs, true);
 				}
 			}
 		} catch (SQLException e) {
-
 			e.printStackTrace();
 		}
 		return null;
--- src/com/yi/handler/user/UserLoginHandler.java
+++ src/com/yi/handler/user/UserLoginHandler.java
@@ -3,6 +3,10 @@ package com.yi.handler.user;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.mysql.jdbc.Connection;
+import com.yi.dao.MemberDao;
+import com.yi.dao.impl.MemberDaoImpl;
+import com.yi.jdbc.JDBCUtil;
 import com.yi.mvc.CommandHandler;
 
 public class UserLoginHandler implements CommandHandler {
@@ -12,6 +16,13 @@ public class UserLoginHandler implements CommandHandler {
 		if(req.getMethod().equalsIgnoreCase("get")) {
 			return "/WEB-INF/view/user/userLogin.jsp";
 		}else if(req.getMethod().equalsIgnoreCase("post")) {
+			String id = req.getParameter("id");
+			String password = req.getParameter("password");
+			
+			Connection conn = null;
+			
+		
+			
 			
 		}
 		return null;
