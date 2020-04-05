e26c9911d508e9567abf26b2ced36bc6f1595f5c
bus503 <bus503@daum.net>
2020-04-05 22:17:50 +0900

    아이디, 패스워드 찾기 기능 구현완료 및 회원가입 구현중

--- src/com/yi/handler/user/UserFindPwHandler.java
+++ src/com/yi/handler/user/UserFindPwHandler.java
@@ -1,20 +1,78 @@
 package com.yi.handler.user;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.yi.dao.LibrarianDao;
+import com.yi.dao.MemberDao;
+import com.yi.dao.impl.LibrarianDaoImpl;
+import com.yi.dao.impl.MemberDaoImpl;
+import com.yi.model.Librarian;
+import com.yi.model.Member;
 import com.yi.mvc.CommandHandler;
 
 public class UserFindPwHandler implements CommandHandler {
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
-	if(req.getMethod().equalsIgnoreCase("get")) {
-			
-	}else if(req.getMethod().equalsIgnoreCase("post")) {
-			
-	}
+		if (req.getMethod().equalsIgnoreCase("get")) {
+
+			return "/WEB-INF/view/user/userFindPw.jsp";
+
+		} else if (req.getMethod().equalsIgnoreCase("post")) {
+
+			try {
+				String id = req.getParameter("id");
+				String name = req.getParameter("name");
+				String birthday = req.getParameter("birthday");
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				Date brthdy = sdf.parse(birthday);
+
+				MemberDao userDao = MemberDaoImpl.getInstance();
+				Member member = new Member();
+				member.setMberId(id);
+				member.setMberName(name);
+				member.setMberBrthdy(brthdy);
+
+				LibrarianDao libDao = LibrarianDaoImpl.getInstance();
+				Librarian libratian = new Librarian();
+				libratian.setLbId(id);
+				libratian.setLbName(name);
+				libratian.setLbBirthDay(brthdy);
+
+				Member findMemberPw = userDao.findMemberPw(member);
+				Librarian findLibPw = libDao.findLibrarianPw(libratian);
+
+				if (findMemberPw == null) {
+					if (findLibPw == null) {
+						HttpSession session = req.getSession();
+						session.setAttribute("error", "nullId");
+
+						return "/WEB-INF/view/user/userFindId.jsp";
+					}
+
+					HttpSession session = req.getSession();
+					session.setAttribute("Lib", findLibPw.getLbPass());
+					res.sendRedirect(req.getContextPath() + "/user/findPw.do");
+
+					return null;
+
+				}
+				HttpSession session = req.getSession();
+				session.setAttribute("Mem", findMemberPw.getMberPass());
+				res.sendRedirect(req.getContextPath() + "/user/findPw.do");
+
+				return null;
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
 		return null;
 	}
-
 }
---
+++ src/com/yi/handler/user/UserJoinHandler.java
@@ -0,0 +1,22 @@
+package com.yi.handler.user;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yi.mvc.CommandHandler;
+
+public class UserJoinHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		if(req.getMethod().equalsIgnoreCase("get")) {
+			
+			return "/WEB-INF/view/user/userJoin.jsp";
+			
+		}else if(req.getMethod().equalsIgnoreCase("post")) {
+			
+		}
+		return null;
+	}
+
+}
